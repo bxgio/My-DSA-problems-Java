ae3b431d57bee21ff881c6eb8004b0a992325849
bxgio <114640185+bxgio@users.noreply.github.com>
2024-01-23 15:52:25 +0530

    modified RemoveDupe Program

--- RemoveDupe.java
+++ RemoveDupe.java
@@ -1,16 +1,20 @@
+
+
+import java.util.*;
 class RemoveDupe {
     public static void main(String[] args) {
         int[] arr ={1,3,4,5,6,6,7,7,7,8};
         System.out.println(removeDuplicates(arr));
+        
+      
     }
-    public static int removeDuplicates(int[] nums) {
-        int j = 1;
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] != nums[i - 1]) {
-                nums[j] = nums[i];
-                j++;
-            }
+    public static HashSet<Integer> removeDuplicates(int[] nums) {
+        HashSet <Integer> hash = new HashSet<>();
+        
+        for(int i=0;i<nums.length;i++){
+            hash.add(nums[i]);
         }
-        return j;
+
+        return hash;
     }
 }
